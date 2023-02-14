6af0d7af5c14ffcdb889bdf7475b7fc14554a2a4
David Martínez <goddrinksjava@gmail.com>
2023-02-13 21:17:30 -0600

    Actividad 4: Búsqueda paralela de archivos

--- app/src/main/java/paralela/App.java
+++ app/src/main/java/paralela/App.java
@@ -4,20 +4,89 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.RecursiveAction;
+import java.util.stream.Stream;
 
 public class App {
   public static void main(String[] args) {
     try {
-      searchFiles(new File("/home/goddr/Pictures"), ".jpg");
+      long start, end;
+
+      File f = new File("D:/");
+      String pattern = ".dll";
+
+      start = System.nanoTime();
+      FileFinder.find(f, pattern);
+      end = System.nanoTime();
+      System.out.println("\n\nTiempo transcurrido (secuencial): " + (end - start) / 1_000_000d + "ms\n\n");
+
+      start = System.nanoTime();
+      new ParallelFileFinder(f, pattern).invoke();
+      end = System.nanoTime();
+      System.out.println("\n\nTiempo transcurrido (paralela): " + (end - start) / 1_000_000d + "ms\n\n");
+
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
+}
 
-  private static void searchFiles(File file, String pattern) throws IOException {
+class ParallelFileFinder extends RecursiveAction {
+  private File file;
+  private String pattern;
+
+  ParallelFileFinder(File file, String pattern) {
+    this.file = file;
+    this.pattern = pattern;
+  }
+
+  @Override
+  protected void compute() {
     if (file.isDirectory()) {
-      for (File f : file.listFiles()) {
-        searchFiles(f, pattern);
+      File[] files = file.listFiles();
+
+      if (files == null) {
+        return;
+      }
+
+      invokeAll(Stream
+          .of(files)
+          .map(f -> new ParallelFileFinder(f, pattern))
+          .toList());
+      return;
+    }
+
+    if (file.getName().contains(pattern)) {
+      BasicFileAttributes bfa;
+      try {
+        bfa = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+        String creationTime = bfa.creationTime().toString();
+
+        System.out.println(
+            "Nombre: " + file.getName() + '\n' +
+                "Ruta: " + file.getAbsolutePath() + '\n' +
+                "Fecha de creación: " + creationTime + '\n' +
+                "Tamaño: " + file.length() + " bytes" + '\n');
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+}
+
+class FileFinder {
+  public static void find(File file, String pattern) throws IOException {
+    if (file.isDirectory()) {
+
+      File[] files = file.listFiles();
+
+      if (files == null) {
+        return;
+      }
+
+      for (File f : files) {
+        find(f, pattern);
       }
       return;
     }
@@ -26,11 +95,11 @@ public class App {
       BasicFileAttributes bfa = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
       String creationTime = bfa.creationTime().toString();
 
-      System.out.println("Nombre: " + file.getName());
-      System.out.println("Ruta: " + file.getAbsolutePath());
-      System.out.println("Fecha de creación: " + creationTime);
-      System.out.println("Tamaño: " + file.length() + " bytes");
-      System.out.println();
+      System.out.println(
+          "Nombre: " + file.getName() + '\n' +
+              "Ruta: " + file.getAbsolutePath() + '\n' +
+              "Fecha de creación: " + creationTime + '\n' +
+              "Tamaño: " + file.length() + " bytes" + '\n');
     }
   }
 }
